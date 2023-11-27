6830e6ad4bb5dc41462c66d19da8a55db203729e
Syd Amir <hemedani@gmail.com>
2023-11-27 13:23:46 +0330

    add lesan app

--- apps/lesan/src/handler.ts
+++ apps/lesan/src/handler.ts
@@ -1,6 +1,6 @@
 // deno-lint-ignore require-await
 export async function handler(_request: Request): Promise<Response> {
-  const message = JSON.stringify({ message: 'Hello lesan' });
+  const message = JSON.stringify({ message: 'hello first deno app' });
   return new Response(message, {
     status: 200,
     headers: {
