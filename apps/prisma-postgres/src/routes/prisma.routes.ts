eea6e7d5f56b5a6362f32b9f0cf123dc01b6a220
emirom <meyari73@gmail.com>
2023-11-27 18:05:50 +0330

    feat: add country routes

---
+++ apps/prisma-postgres/src/routes/prisma.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express"
+import createCountry from "../controllers/country/createCountry.controller"
+const router = Router()
+
+/**
+ *  also the crud of all entities are available in controller folder
+ *  we add bench mark routes
+ *  */
+router.post( "country/create", createCountry )
+export default router
